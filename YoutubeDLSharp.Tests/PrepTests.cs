2c61f287938fa03e743138466478718e43e106b0
Micah <micah686@github.com>
2022-09-04 14:09:28 -0700

    Added download helper

---
+++ YoutubeDLSharp.Tests/PrepTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace YoutubeDLSharp.Tests
+{
+    internal static class PrepTests
+    {
+        private static bool _didDownloadBinaries = false;
+
+        internal static void DownloadBinaries()
+        {
+            if (_didDownloadBinaries == false)
+            {
+                if (!File.Exists("yt-dlp.exe"))
+                {
+                    YoutubeDL.DownloadYtDlpBinary();
+                }
+                if (!File.Exists("ffmpeg.exe"))
+                {
+                    YoutubeDL.DownloadFFmpegBinary();
+                }
+                _didDownloadBinaries = true;
+            }
+        }
+    }
+}
